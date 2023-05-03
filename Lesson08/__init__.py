f87d913f8109f227e1f77af387006ed2ee13585f
AlexKri72 <krial11@rambler.ru>
2023-05-03 19:56:26 +0500

    Homework Lesson 08 is ready

---
+++ Lesson08/__init__.py
@@ -0,0 +1,11 @@
+from Task01 import json_trans
+from Task02 import safe_user_data
+from Task03 import safe_json_to_csv
+from Task04 import func
+from Task05 import json_to_pickle
+from Task06 import pickle_to_csv
+from Task07 import read_csv
+from Homework01 import get_size, get_dir_size, direct_info
+
+__all__ = ['json_trans', 'safe_user_data', 'safe_json_to_csv', 'func', 'json_to_pickle', 'pickle_to_csv', 'read_csv',
+           'get_size', 'get_dir_size', 'direct_info']
